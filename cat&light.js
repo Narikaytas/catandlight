de476535021a7b991ad7630019d6d6f2b8a68ebf
Satyakiran <95997405+Narikaytas@users.noreply.github.com>
2022-04-27 07:09:55 +0530

    Update cat&light.js
    Java Script added

--- cat&light.js
+++ cat&light.js
@@ -1,15 +1,27 @@
- let On = document.getElementById('On');
- let Off = document.getElementById('Off');
+ let switchOn = document.getElementById('switchOn');
+ let switchOff = document.getElementById('switchOff');
+ let bulbStatus = document.getElementById('bulbStatus');
 //  let bulb = document.getElementById('bulb');
 //  let cat = document.getElementById('cat');
  
 
- function switchOff(){
+ function switchOFF(){
       document.getElementById('bulb').src ="images/bulb-go-off-img.png";
       document.getElementById('cat').src ="images/cat-eyes-img.png";
+      document.getElementById('bulbStatus').innerHTML ="Swiched OFF";
+      document.getElementById('switchOn').style.background = "Red";
+      document.getElementById('switchOff').style.background = "Green";
+     //   
  }
 
- function switchOn(){
-    document.getElementById('bulb').src ="images/bulb-go-on-img.png";
-    document.getElementById('cat').src ="images/cat-img.png";
+ function switchON(){
+     document.getElementById('bulb').src ="images/bulb-go-on-img.png";
+     document.getElementById('cat').src ="images/cat-img.png"; 
+     document.getElementById('bulbStatus').innerHTML ="Swiched ON";
+     document.getElementById('switchOn').style.background = "Green";
+     document.getElementById('switchOff').style.background = "red";
+     
+     
+
 }
+ 
